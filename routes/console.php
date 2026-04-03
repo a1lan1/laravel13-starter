f53f3bd6c73ccdf80b17037e26ccf2a69975a15a
Aleksandr Smirnov <genesboard@gmail.com>
2026-04-03 15:38:59 +0300

    add spatie/laravel-backup

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,9 @@
 
 use Illuminate\Support\Facades\Schedule;
 
+// Telescope
 Schedule::command('telescope:prune')->everyTwoHours();
+
+// Backup
+Schedule::command('backup:run')->daily()->at('08:00');
+Schedule::command('backup:clean')->daily()->at('20:00');

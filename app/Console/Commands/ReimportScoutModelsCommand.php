3d4b9c5674add7a8a3e8d0feb7a64ba873e70af2
Aleksandr Smirnov <genesboard@gmail.com>
2026-04-26 12:46:54 +0300

    add reimport scout models artisan command

---
+++ app/Console/Commands/ReimportScoutModelsCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ReimportScoutModelsCommand extends Command
+{
+    protected $signature = 'scout:reimport-all';
+
+    protected $description = 'Re-imports all searchable models';
+
+    /**
+     * @var array<class-string>
+     */
+    protected array $searchableModels = [
+        //
+    ];
+
+    public function handle(): int
+    {
+        $this->info('Starting re-import of all searchable models...');
+
+        foreach ($this->searchableModels as $model) {
+            $this->line(sprintf('Importing <comment>%s</comment>...', $model));
+
+            Artisan::call('scout:import', ['model' => $model], $this->getOutput());
+
+            $this->info(sprintf('Finished importing <comment>%s</comment>.', $model));
+        }
+
+        $this->info('All searchable models have been re-imported successfully!');
+
+        return self::SUCCESS;
+    }
+}
